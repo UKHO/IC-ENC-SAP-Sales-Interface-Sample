d74e92e490aca3aee0662be7df1e398675d3eee3
Andrew Slater <spiraljacobs@gmail.com>
2022-07-25 14:13:25 +0100

    184894 removing clientmessageinspector, adding subscriptionkey as query param.
     removing clientmessageinspector, adding subscriptionkey as query param.

--- SalesInterfaceServiceClient/SapClient.cs
+++ SalesInterfaceServiceClient/SapClient.cs
@@ -9,19 +9,17 @@ namespace SalesInterfaceServiceClient
         private const string Url = "sales_interface_service_environment_url_goes_here";
 
         // change this to your subscription key
-        private const string SubscriptionKey = "your_subscription_key_goes_here_without_ending_foreslash";
+        private const string SubscriptionKey = "your_subscription_key_goes_here";
 
         public z_sales_interfaceClient CreateSapClient()
         {
             var binding = CreateBasicHttpBinding();
 
             var endpoint = new EndpointAddress(
-                new Uri(Url + "/?soapAction=https://engenavigator.engineering.ukho.gov.uk/1.0/IOrderingService/SubmitOrder")
+                new Uri(Url + "/?soapAction=https://engenavigator.engineering.ukho.gov.uk/1.0/IOrderingService/SubmitOrder&subscription-key=" + SubscriptionKey)
             );
             
-
             var client = new z_sales_interfaceClient(binding, endpoint);
-            client.Endpoint.EndpointBehaviors.Add(new ClientMessageInspector(SubscriptionKey));
             return client;
         }
 
